7afeed6fd8f57c2f1e9e36373114d136312d3ca6
Daniel Orozco <00200617@uca.edu.sv>
2018-05-01 13:22:57 -0600

    Clase Huesped que hereda de Clase Tarjeta

---
+++ src/parcialpoo/Huesped.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+/**
+ *
+ * @author Daniel Alejandro Orozco Orellana <00200617@uca.edu.sv>
+ */
+public class Huesped{ //extends Tarjeta{
+    public String contacto;
+    private int idHuesped;
+
+    public Huesped(){
+
+    }
+
+
+
+}
